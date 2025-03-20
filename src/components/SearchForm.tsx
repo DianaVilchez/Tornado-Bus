f7e45ceb78bd4e024c547003935a6707e07d0ed2
Diana <educ.dana@gmail.com>
2025-03-20 18:08:17 -0500

    feat: implement SearchForm component

---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,96 @@
+import { Formik, Form, Field, ErrorMessage, FieldProps } from "formik";
+import * as Yup from "yup";
+import CityField from "./CityField";
+import { useState } from "react";
+
+const searchSchema = Yup.object({
+  originCity: Yup.string().required("La ciudad de origen es obligatoria"),
+  destinationCity: Yup.string().required("La ciudad de destino es obligatoria"),
+  travelDate: Yup.date()
+    .min(new Date(), "La fecha no debe ser menor a la de hoy")
+    .required("La fecha de viaje es obligatoria"),
+  passengers: Yup.number()
+    .min(1, "Debe de seleccionar almenos un pasajero")
+    .required("El numero de pasajero es obligatorio"),
+});
+
+
+export default function SearchForm() {
+  const [cityInitId, setCityInitId] = useState<number |  undefined>(undefined);
+
+  return (
+    <Formik
+      initialValues={{
+        originCity: "",
+        destinationCity: "",
+        travelDate: "",
+        passengerCount: 1,
+      }}
+      validationSchema={searchSchema}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+      validateOnChange={false}
+      validateOnBlur={false}
+    >
+      {(formikProps) => (
+        <Form className="form-container">
+          <div className="form-options">
+            <label> Ciudad de Origen:</label>
+            <Field name="originCity">
+              {({ field, form, meta }: FieldProps) => (
+                <CityField
+                  field={field}
+                  form={form}
+                  meta={meta}
+                  type="origin"
+                  cityInitId={cityInitId}
+                  onSelectCity={(city) => {
+                    setCityInitId(city.id);
+                    formikProps.setFieldValue("originCity", city);
+                  }}
+                />
+              )}
+            </Field>
+            <ErrorMessage name="originCity" />
+          </div>
+
+          <div className="form-options">
+            <label> Ciudad de Destino:</label>
+            <Field name="destinationCity">
+              {({ field, form, meta }: FieldProps) => (
+                <CityField
+                  field={field}
+                  form={form}
+                  meta={meta}
+                  type="destination"
+                  cityInitId={cityInitId} 
+                  onSelectCity={(city: { id: number; name: string }) => {
+                    setCityInitId(city.id);
+                    form.setFieldValue("destinationCity", city);
+                  }}
+                />
+              )}
+            </Field>
+
+            <ErrorMessage name="destinationCity" />
+          </div>
+
+          <div className="form-options">
+            <label> Fecha de Viaje:</label>
+            <Field type="date" id="travelDate" name="travelDate"/>
+            <ErrorMessage name="travelDate" />
+          </div>
+
+          <div className="form-options">
+            <label> Pasajeros:</label>
+            <Field type="number" id="passengers" name="passengers" />
+            <ErrorMessage name="passengers" />
+          </div>
+
+          <button type="submit">Buscar</button>
+        </Form>
+      )}
+    </Formik>
+  );
+}

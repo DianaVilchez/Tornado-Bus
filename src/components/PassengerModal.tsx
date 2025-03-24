b2bcce9f89b05c7fc2634f6aea7ddb9b2b953b78
Diana <educ.dana@gmail.com>
2025-03-24 03:39:23 -0500

    style: update styles and formatting in PassengerModal

--- src/components/PassengerModal.tsx
+++ src/components/PassengerModal.tsx
@@ -2,7 +2,7 @@ import { Dialog, Switch } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const label = { inputProps: { 'aria-label': 'Switch demo' } };
+const label = { inputProps: { "aria-label": "Switch demo" } };
 
 interface Passenger {
   id: number;
@@ -16,14 +16,18 @@ interface PassengerType {
   passenger: Passenger[];
 }
 interface PassengerModalProps {
-  open:boolean;
+  open: boolean;
   onClose: () => void;
   onTotalChange: (total: number) => void;
 }
 
-export default function PassengerModal({ open ,onClose, onTotalChange }: PassengerModalProps) {
+export default function PassengerModal({
+  open,
+  onClose,
+  onTotalChange,
+}: PassengerModalProps) {
   const [types, setTypes] = useState<PassengerType[]>([]);
-  
+
   useEffect(() => {
     axios
       .get("https://api.local.onroadts.com/v1/web/select/type")
@@ -42,88 +46,120 @@ export default function PassengerModal({ open ,onClose, onTotalChange }: Passeng
       })
       .catch((error) => console.error("Error obteniendo datos", error));
   }, []);
-  
+
   const addPassenger = (typeIndex: number) => {
     setTypes((prev) => {
-      const newPassenger = prev.map((type, i) => 
-      i === typeIndex 
-        ? {...type, passenger : [...type.passenger, {id: type.passenger.length +1, disabled: false }]}
-        :type
-    );
-    console.log("🚀 Pasajero agregado:", newPassenger);
-    return newPassenger;
-  });
-  }
+      const newPassenger = prev.map((type, i) =>
+        i === typeIndex
+          ? {
+              ...type,
+              passenger: [
+                ...type.passenger,
+                { id: type.passenger.length + 1, disabled: false },
+              ],
+            }
+          : type
+      );
+      console.log("🚀 Pasajero agregado:", newPassenger);
+      return newPassenger;
+    });
+  };
 
   const removePassenger = (typeIndex: number) => {
     setTypes((prev) => {
-      const lessPassenger = prev.map((type, i) => 
-      i === typeIndex
-      ? {...type, passenger : type.passenger.slice(0,-1)}
-      :type
-    );
-    console.log("🚀 Pasajero quitado:", lessPassenger);
-    return lessPassenger;
-  })
-  }
+      const lessPassenger = prev.map((type, i) =>
+        i === typeIndex
+          ? { ...type, passenger: type.passenger.slice(0, -1) }
+          : type
+      );
+      console.log("🚀 Pasajero quitado:", lessPassenger);
+      return lessPassenger;
+    });
+  };
 
-  const toggleDisabled = (typeIndex:number, passengerId:number) => {
+  const toggleDisabled = (typeIndex: number, passengerId: number) => {
     setTypes((prev) => {
-      const disabled = prev.map((type, i) => 
-      i === typeIndex
-      ? {...type, 
-      passenger : type.passenger.map((p) => 
-        p.id === passengerId ?
-        {...p, disabled: !p.disabled} : p
-      ),
-    }
-    :type
-  )
-  console.log("🚀 Pasajero con Discapacidad:", disabled)
-  return disabled
-}
-);
-}
-const handleContinue = () => {
-  let totalPassengers = 0;
+      const disabled = prev.map((type, i) =>
+        i === typeIndex
+          ? {
+              ...type,
+              passenger: type.passenger.map((p) =>
+                p.id === passengerId ? { ...p, disabled: !p.disabled } : p
+              ),
+            }
+          : type
+      );
+      console.log("🚀 Pasajero con Discapacidad:", disabled);
+      return disabled;
+    });
+  };
+  const handleContinue = () => {
+    let totalPassengers = 0;
 
-  types.forEach((type) => {
-    totalPassengers += type.passenger.length;
-  });
-  console.log(`${totalPassengers} pasajeros`);
+    types.forEach((type) => {
+      totalPassengers += type.passenger.length;
+    });
+    console.log(`${totalPassengers} pasajeros`);
 
-  onTotalChange(totalPassengers);
-  onClose();
-
-};
+    onTotalChange(totalPassengers);
+    onClose();
+  };
   return (
     <Dialog open={open} onClose={onClose}>
-    <section>
-      <button onClick={onClose}>x</button>
-      {types.map((type, typeIndex) => (
-        <div key={type.id}>
-          <h2>{type.name}</h2>
-          <h3>De {type.ageMin} a {type.ageMax} años </h3>
-          <button onClick={() => addPassenger(typeIndex)}> + </button>
-          <button onClick={() => removePassenger(typeIndex)}> - </button>
-          <div>
-            {type.passenger.map((passenger) => (
-              <div key={passenger.id}>
-                {type.name} {passenger.id}
-                <Switch {...label}
-                // checked={passenger.disabled}
-                onChange={() => toggleDisabled(typeIndex, passenger.id)}
-                />♿
+      <section className="modal-Passengers">
+        <button style={{ right: "0" }} onClick={onClose}>x</button>
+        {types.map((type, typeIndex) => (
+          <div key={type.id}>
+            <div className="count-typePassenger">
+              <div>
+                <h2
+                  style={{
+                    fontFamily: "'Martel Sans',sans-serif",
+                    fontSize: "18px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {type.name.toUpperCase()}
+                </h2>
+                <h3>
+                  De {type.ageMin} a {type.ageMax} años{" "}
+                </h3>
               </div>
-            ))}
-            
+              <div>
+                <button
+                  style={{ backgroundColor: "#FB8A00" }}
+                  onClick={() => addPassenger(typeIndex)}
+                >
+                  {" "}
+                  +{" "}
+                </button>
+                <button
+                  style={{ backgroundColor: "#FB8A00", marginLeft:"20px" }}
+                  onClick={() => removePassenger(typeIndex)}
+                >
+                  {" "}
+                  -{" "}
+                </button>
+              </div>
+            </div>
+            <div>
+              {type.passenger.map((passenger) => (
+                <div key={passenger.id}>
+                  {type.name.toUpperCase()} {passenger.id}
+                  <Switch
+                    {...label}
+                    // checked={passenger.disabled}
+                    onChange={() => toggleDisabled(typeIndex, passenger.id)}
+                  />
+                  ♿
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        
-      ))}
-      {/* <p>{total}</p> */}
-      <button onClick={handleContinue} > Continuar </button>
-    </section>
+        ))}
+        {/* <p>{total}</p> */}
+        <button className="button-search" onClick={handleContinue}> Continuar </button>
+      </section>
     </Dialog>
   );
 }

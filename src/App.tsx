a6dfcf75ac23962f08165dc03ada3e85a758f2fd
Diana <educ.dana@gmail.com>
2025-03-24 10:10:02 -0500

    feat: add new route and reorganize routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import SearchForm from "./components/SearchForm";
 import SearchResults from "./components/SearchResults";
+import MainView from "./components/MainView";
+import TravelDetails from "./components/TravelDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SearchForm />} />
+        <Route path="/" element={<MainView />} />
         <Route path="/results" element={<SearchResults />} />
+        <Route path="/details" element={<TravelDetails />} />
       </Routes>
     </BrowserRouter>
   );

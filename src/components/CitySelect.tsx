263eee4c78891af575caa53cf4315e5f0af7b913
Diana <educ.dana@gmail.com>
2025-04-06 12:42:32 -0500

    feat: replace CityField with CitySelect using react-select

---
+++ src/components/CitySelect.tsx
@@ -0,0 +1,33 @@
+import AsyncSelect from "react-select/async";
+import { City, cityService } from "../services/cityService";
+
+type CitySelect = {
+  type: "origin" | "destination";
+  originCityId?: number;
+  onChange: (value: City | null ) => void; 
+}
+export const CitySelect = ({
+  type,
+  originCityId,
+  onChange,
+}: CitySelect) => {
+const loadOptions = async (inputValue: string) => {
+      const response = await cityService({query:inputValue, type, cityInitId:originCityId});
+  return response.map((city) => ({label: city.name, value: city.id}));
+}
+return (
+  <AsyncSelect
+  defaultOptions={true}
+  loadOptions={loadOptions}
+  onChange={(option) => {
+          if (option) {
+            onChange({ id: option.value, name: option.label });
+          } else {
+            onChange(null);
+          }
+        }}
+        isDisabled={type === "destination" && !originCityId}
+    placeholder={type === "origin" ? "Selecciona origen" : "Selecciona destino"}
+  />
+)
+}

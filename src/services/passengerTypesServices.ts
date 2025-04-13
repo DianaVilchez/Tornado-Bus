a457763438cb2478d008d7f46fb7863c068d0cdd
Diana <educ.dana@gmail.com>
2025-04-13 09:23:31 -0500

    refactor: separate API logic into passengerTypesServices

---
+++ src/services/passengerTypesServices.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+export interface Passenger {
+    id: number;
+    disabled: boolean;
+  }
+export interface PassengerType {
+    id: number;
+    name: string;
+    ageMin: number;
+    ageMax: number;
+    passenger: Passenger[];
+  }
+
+export const getPassengerTypes = async (): Promise<PassengerType[]> => {
+    try{
+      const response = await axios.get("https://api.local.onroadts.com/v1/web/select/type");
+      const data = response.data.data;
+      console.log("Datos API", response.data.data);
+
+      const formatted = data.map((type: any) => ({
+        id: type.id,
+        name: type.name,
+        ageMin: type.ageMin,
+        ageMax: type.ageMax,
+        passenger: [],
+      }));
+      return(formatted);
+}
+  catch(error){
+    console.error("Error obteniendo tipos de pasajero", error);
+    throw error;
+  }
+}
